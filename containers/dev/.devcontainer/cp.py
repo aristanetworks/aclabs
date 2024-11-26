bbdba2868a234aefb566f161ea92983fc4296700
Petr Ankudinov <pa@arista.com>
2024-11-26 14:03:06 +0100

    First templating tests (#62)
    - add Copier-based templating
    - add CVaaS EVPN MLAG lab to acLabs
    - move telemetry lab

---
+++ containers/dev/.devcontainer/cp.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+
+import copier
+import sys
+import csv
+import yaml
+import os
+import shutil
+import argparse
+import re
+
+def read_csv_file(filename):
+    with open(filename, mode='r') as csv_file:
+        csv_row_dict_list = list()  # list of key-value pairs produced from every CSV row except header
+        # if header contains __CCvar and __CCvalue CSV will be processed vertically
+        # each row will be treated as separate variable with a name of __CCvar
+        vars_from_csv = dict()
+        for row in csv.DictReader(csv_file):
+            updated_row_dict = dict()
+            for k, v in row.items():
+                # remove potential spaces left and right
+                k = k.strip()
+                if v:
+                    v = v.strip()
+                updated_row_dict.update({k: v})
+            if '__CCkey' in updated_row_dict.keys():
+                if not '__CCvalue' in updated_row_dict.keys():
+                    sys.exit(
+                        f'ERROR: __CCkey is defined without __CCvalue in {csv_file}')
+                vars_from_csv.update({updated_row_dict['__CCkey']: updated_row_dict['__CCvalue']})
+            else:
+                csv_row_dict_list.append(updated_row_dict)
+
+    if len(csv_row_dict_list):
+        return csv_row_dict_list
+    else:
+        return vars_from_csv
+
+def read_yaml_file(filename, load_all=False):
+    with open(filename, mode='r') as f:
+        if not load_all:
+            yaml_data = yaml.load(f, Loader=yaml.FullLoader)
+        else:
+            # convert generator to list before returning
+            yaml_data = list(yaml.load_all(f, Loader=yaml.FullLoader))
+    return yaml_data
+
+def load_extra_vars(data_input_directory):
+
+    extra_vars = dict()
+    # load all data from input directory and assign to corresponding dict keys
+    data_input_directory_full_path = os.path.join(
+        os.getcwd(), data_input_directory)
+    if not os.path.isdir(data_input_directory_full_path):
+        sys.exit(
+            f'ERROR: Can not find data input directory {data_input_directory_full_path}')
+    
+    # read files from the data input directory and add to extra_vars
+    # every file will be added as dictionary with a filename without extension as the parent key
+    for a_name in os.listdir(data_input_directory_full_path):
+        a_full_path = os.path.join(data_input_directory_full_path, a_name)
+        if os.path.isfile(a_full_path):
+            if '.csv' in a_name.lower():
+                csv_data = read_csv_file(a_full_path)
+                
+                extra_vars.update({
+                    # [:-4] removes .csv
+                    a_name.lower()[:-4]: csv_data
+                })
+            elif '.yml' in a_name.lower():
+                data_from_yaml = read_yaml_file(a_full_path)
+                extra_vars.update({
+                    # [:-4] removes .yml
+                    a_name.lower()[:-4]: data_from_yaml
+                })
+            elif '.yaml' in a_name.lower():
+                data_from_yaml = read_yaml_file(a_full_path)
+                extra_vars.update({
+                    # [:-5] removes .yaml
+                    a_name.lower()[:-5]: data_from_yaml
+                })
+
+    return extra_vars
+
+if __name__ == "__main__":
+
+    default_template_dir = '.cp'
+    default_input_dir = '.cp/extra-vars/default'
+    temp_template_dir = '.cp-temp'
+
+    # get directory to load extra context
+    parser = argparse.ArgumentParser(
+        prog="copy",
+        description="Init new lab from template.")
+    parser.add_argument(
+        '-in', '--input_directory', default=default_input_dir,
+        help='Directory with CSV or YAML files to load as extra context'
+    )
+    parser.add_argument(
+        '-d', '--debug', action='store_true', default=False,
+        help='Debug the templating tool. This will save all files in a dedicated directory.'
+    )
+    args = parser.parse_args()
+
+    if args.debug:
+        copier_dst_directory = 'temp'
+    else:
+        copier_dst_directory = '.'
+
+    extra_vars = load_extra_vars(args.input_directory)
+
+    files_to_copy = list()
+    file_index_list = list()
+    # find template files and for loops
+    for root, _, files in os.walk(default_template_dir):
+        for filename in files:
+            full_src_path = os.path.join(root, filename)
+            f, extension = os.path.splitext(filename)
+            if extension == ".jinja" and re.match(r"^\{%.*for", f):
+                re_pattern = r"(?:^\{%\s* for\s*)(\w+)(?:\s*in\s*)(\w+)(?:\s*%\}).(\w+)$"
+                loop_key = re.sub(re_pattern, r"\1", f)
+                loop_over = extra_vars[re.sub(re_pattern, r"\2", f)]
+                true_ext = re.sub(re_pattern, r".\3.jinja", f)
+                for i, d in enumerate(loop_over):
+                    file_index_list.append((os.path.join(root, d[loop_key]+true_ext).replace(default_template_dir, temp_template_dir), i))
+                    files_to_copy.append((full_src_path, os.path.join(root, d[loop_key]+true_ext).replace(default_template_dir, temp_template_dir)))
+            else:
+                files_to_copy.append((full_src_path, full_src_path.replace(default_template_dir, temp_template_dir)))
+
+    for src_file, dst_file in files_to_copy:
+        os.makedirs(os.path.dirname(dst_file), exist_ok=True)
+        shutil.copy(src=src_file, dst=dst_file)
+
+    for indexed_file, index in file_index_list:
+        with open(indexed_file) as f:
+            data = f.read()
+        with open(indexed_file, 'w') as f:
+            f.write("{%- set copier_file_index = "+f"{index}"+" -%}\n"+data)
+
+    # run copier
+    cp = copier
+    cpWorker = cp.Worker(src_path=temp_template_dir, dst_path=copier_dst_directory, data=extra_vars, unsafe='True')
+    cpWorker.run_copy()
+
+    shutil.rmtree(temp_template_dir)
---
+++ docs/cvaas-cvaas-and-avd-demo--evpn-mlag.md
@@ -0,0 +1,104 @@
+# CVaaS and AVD Demo, EVPN MLAG
+
+!!! Warning "Container Requirements"
+
+    :fontawesome-solid-microchip: CPUs: 8  
+    :fontawesome-solid-memory: Memory: 32 GB  
+    :fontawesome-solid-hard-drive: Storage: 64 GB  
+
+    :material-alert-circle-outline:{ .heartbeat } Please request high spec Codespace machines from [Github support](https://support.github.com/) first!
+
+[Slides](https://{{gh.org_name}}.github.io/{{gh.repo_name}}/slides/cvaas-cvaas-and-avd-demo--evpn-mlag.html){ target=_blank }  
+[PDF Slides](https://{{gh.org_name}}.github.io/{{gh.repo_name}}/pdfs/cvaas-cvaas-and-avd-demo--evpn-mlag.pdf){ target=_blank }
+
+## How To Run The Lab
+
+Please read the [Quickstart guide](https://ankudinov.github.io/aclabs/quickstart/) before using the lab.
+
+You can run the lab on [Github Codespaces](https://codespaces.new/{{gh.repository}}/tree/{{gh.branch}}?quickstart=1&devcontainer_path=.devcontainer%2Fcvaas-cvaas-and-avd-demo--evpn-mlag%2Fdevcontainer.json) or your own machine.
+
+[Start :octicons-play-16: "CVaaS and AVD Demo, EVPN MLAG" in Codespace :octicons-mark-github-24:](https://codespaces.new/{{gh.repository}}/tree/{{gh.branch}}?quickstart=1&devcontainer_path=.devcontainer%2Fcvaas-cvaas-and-avd-demo--evpn-mlag%2Fdevcontainer.json){ .md-button .md-button--primary target=_blank}
+
+To run the lab on your own machine, you can download all required files using the button below.
+
+[Download all required lab files](https://{{gh.org_name}}.github.io/aclabs/lab_archives/cvaas-cvaas-and-avd-demo--evpn-mlag.tar.gz){ .md-button .md-button--primary target=_blank}
+
+> WARNING: Currently only x86 hosts are supported. cEOS-lab for ARM is not yet available and it's not possible to start the lab on your MacBook yet.
+
+!!! info "Environment variables and secrets"
+
+    This lab requires following environment variables and secrets to be set.
+
+    `ARISTA_TOKEN` - the token required to download cEOS-lab image from [arista.com](https://arista.com).
+    `CV_API_TOKEN` - CVaaS service account token. It's required to generate onboarding token on CVaaS and allow streaming from the lab devices.
+    `CVURL` (Optional) - URL to be used to access CVaaS. Default is `cv-staging.corp.arista.io`
+
+    When starting the lab on Github Codespaces, the required data can be provided via `Create codespace` form if not yet associated with the repository. When running the lab on your own machine - set corresponding environment variables **BEFORE** :warning: opening the VSCode.
+
+???+ Tip "Wait until the lab is ready"
+
+    When lab container starts, the `postCreate.sh` takes care of cEOS-lab image download and making some last minute changes in the lab environment. After that, `make start` shortcut is executed to start the lab.
+    This requires a some time. :stopwatch: Please be patient. :coffee: :croissant:
+    Sometimes cEOS-lab image download may fail. For example, due to incorrect token. In that case `postCreate.sh` script will fail and the lab will not be started.  
+    You can confirm if image was imported correctly with `docker image ls`.  
+
+## Lab Inventory
+
+This lab has following devices:
+
+| Hostname | Type | OS | Management Address | Login | Password |
+| -------- | ---- | -- | ------------------ | ----- | -------- |
+| s01 | switch | cEOS-lab, 4.32.3M | 10.0.1.1 | arista | arista |
+| s02 | switch | cEOS-lab, 4.32.3M | 10.0.1.2 | arista | arista |
+| l01 | switch | cEOS-lab, 4.32.3M | 10.0.2.1 | arista | arista |
+| l02 | switch | cEOS-lab, 4.32.3M | 10.0.2.2 | arista | arista |
+| l03 | switch | cEOS-lab, 4.32.3M | 10.0.2.3 | arista | arista |
+| l04 | switch | cEOS-lab, 4.32.3M | 10.0.2.4 | arista | arista |
+| h01 | host | cEOS-lab, 4.32.3M | 10.0.3.1 | arista | arista |
+| h02 | host | cEOS-lab, 4.32.3M | 10.0.3.2 | arista | arista |
+
+> To access any device, use `ssh <username>@<hostname>` or simply type `<hostname>` to use the SSH alias.
+
+## Last Updated
+
+!!! Info "Last reviewed: 26/11/2024"
+
+    Demos and labs reviewed over 6 month age may be outdated.
+
+# Lab Topology
+
+![lab topology](assets/topos/small-l3ls-mlag.png)
+
+# How To Use The Lab
+
+???+ Tip "Wait until all devices will start streaming to CVaaS."
+
+    This may take a while.
+
+```bash
+# 1. build configs with AVD
+make build
+# 2. create CVP change control (1)
+make deploy_cvp
+# 3. assign tags for CVP topology view (2)
+make tags
+# 4. validate the deployment with ANTA preview
+make test
+```
+
+1. !!! Tip "Review and execute the change control on CVP when all tasks will be created. If you don't have CVaaS available and prefer to deploy the configuration via eAPI, you can use `make deploy` shortcut instead."
+2. !!! Bug "Currently there is a bug with disabling LLDP on Ma0, which prevents topology view from functioning correctly."
+
+Connect to a host (h01 or h02) and execute `test` alias to confirm connectivity.  
+Execute following commands to verify EVPN control plane:
+
+```text
+show ip bgp summary
+show bgp evpn summary
+show bgp evpn route-type ip-prefix ipv4
+show bgp evpn route-type mac-ip
+```
+
+Do any other testing in the lab.  
+Impress your customer, colleagues or make yourself a bit smarter and happier.  
+You rock! 🚀
