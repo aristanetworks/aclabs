e2b2b06b54018fbe2a6ca814fd041c6329cdc415
Mitch Vaughan <mitch.vaughan@gmail.com>
2024-12-02 17:53:33 +0000

    update postcreate script tl domain a

--- labs/techlib-vxlan-domain-a/postCreate.sh
+++ labs/techlib-vxlan-domain-a/postCreate.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-if [ "${GITHUB_REPOSITORY}" ]; then
-  grep -rl '{{gh.codespace_name}}' . --exclude-dir .git | xargs sed -i 's/{{gh.repo_name}}/'"${CODESPACE_NAME##*/}"'/g'
-  grep -rl '{{gh.codespaces_port_forwarding}}' . --exclude-dir .git | xargs sed -i 's/{{gh.org_name}}/'"${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN%%/*}"'/g'
-fi
+grep -rl '{{gh.codespace_name}}' . --exclude-dir .git | xargs sed -i 's/{{gh.repo_name}}/'"${CODESPACE_NAME##*/}"'/g'
+grep -rl '{{gh.codespaces_port_forwarding}}' . --exclude-dir .git | xargs sed -i 's/{{gh.org_name}}/'"${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN%%/*}"'/g'
