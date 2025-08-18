b48fca61ecdd7707f64f0cd962a8e3fb3c9e6beb
pa <pa@arista.com>
2025-08-18 08:32:28 +0000

    add arm to base container

--- containers/lab-base/.devcontainer/postCreate.sh
+++ containers/lab-base/.devcontainer/postCreate.sh
@@ -23,8 +23,53 @@ else
   grep -rl '{{cv_url}}' . --exclude-dir .git | xargs sed -i 's@{{cv_url}}@'"cv-staging.corp.arista.io"'@g'
 fi
 
-ardl get eos --format cEOS64 --version ${CEOS_LAB_VERSION} --import-docker
-docker tag arista/ceos:${CEOS_LAB_VERSION} arista/ceos:latest
+# for D-in-D case container engine will be always Docker
+# however we do not want to hardcode it for future cases
+if [ "$(command -v podman)" ]; then
+    CONTAINER_ENGINE="podman"
+elif [ "$(command -v docker)" ]; then
+    CONTAINER_ENGINE="docker"
+else
+    echo "ERROR: Failed to find container engine. Please install docker or podman." >&2
+    exit 1
+fi
+
+# always prune old containers a clean lab on laptops
+${CONTAINER_ENGINE} container prune -f
+
+# check if ceos-lab image already present
+if [ -z "$(${CONTAINER_ENGINE} image ls | grep 'arista/ceos')" ]; then
+    if [ "${ARISTA_TOKEN}" ]; then
+        # `uname -m` is used to find platform architecture
+        # currently we check for arm64 and aarch64 and expect everything else to be an x86 machine
+        echo "$(uname -m)"
+        if [ "$(uname -m)" = "aarch64" ] || [ "$(uname -m)" = "arm64" ]; then
+            ardl get eos --image-type cEOSarm --version ${CEOS_LAB_VERSION} --import-docker
+            ${CONTAINER_ENGINE} tag arista/ceos:${CEOS_LAB_VERSION} arista/ceos:latest
+        else
+            ardl get eos --image-type cEOS --version ${CEOS_LAB_VERSION} --import-docker
+            ${CONTAINER_ENGINE} tag arista/ceos:${CEOS_LAB_VERSION} arista/ceos:latest
+        fi
+    else
+        # if ARISTA_TOKEN is not defined - we'll try to pull it from a private container registry
+        if docker pull arista/ceos:${CEOS_LAB_VERSION} >/dev/null 2>&1; then
+            echo "WARNING: pulled cEOS-lab image from the private container registry successfully."
+        else
+            echo "WARNING: arista.com token was not defined and image is not available on the private registry!"
+        fi
+    fi
+else
+    echo "WARNING: cEOS-lab image already present. Skipping the image pull/download."
+fi
+
+# check if image is still missing and print a warning
+if [ -z "$(${CONTAINER_ENGINE} image ls | grep 'arista/ceos')" ]; then
+    echo "WARNING: cEOS-lab image download failed. Try to upload and import it manually."
+    echo "         Please make sure that image is imported with a correct name - arista/ceos:latest"
+    echo "         The lab will not auto start! Run 'make start' when image is ready."
+else
+    make start
+fi
 
 if [ "${CV_API_TOKEN}" ]; then
   if [ "${CVURL}" ]; then
