d34dbc7b259c9a16bbc1a6e30be8c886b853927d
pa <pa@arista.com>
2025-08-21 12:10:20 +0000

    try entrypoint with WSF

--- containers/coder/.devcontainer/code-server-entrypoint.sh
+++ containers/coder/.devcontainer/code-server-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-code-server --bind-addr 0.0.0.0:8080 --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "/cvaas-cvaas-and-avd-demo--evpn-mlag"
+code-server --bind-addr 0.0.0.0:8080 --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}"
