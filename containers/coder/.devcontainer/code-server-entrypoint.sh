d69e1781b0d28c4774327aaf1d99ac9fb707b307
pa <pa@arista.com>
2025-08-29 16:19:29 +0000

    run code-server in background

--- containers/coder/.devcontainer/code-server-entrypoint.sh
+++ containers/coder/.devcontainer/code-server-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-code-server --bind-addr 0.0.0.0:8080 --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}"
+code-server --bind-addr 0.0.0.0:8080 --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}" &
 
 /usr/local/share/docker-init.sh
 
