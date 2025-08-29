6a33f785bf2b28e8606a565a9b99b97a940e437c
pa <pa@arista.com>
2025-08-29 15:02:14 +0000

    add exec to entrypoint

--- containers/coder/.devcontainer/code-server-entrypoint.sh
+++ containers/coder/.devcontainer/code-server-entrypoint.sh
@@ -2,3 +2,19 @@
 set -e
 
 code-server --bind-addr 0.0.0.0:8080 --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}"
+
+if [ -f "/bin/postCreate.sh" ]; then
+    /bin/postCreate.sh
+fi
+
+# Execute command from docker cli if any.
+if [ ${@+True} ]; then
+    exec "$@"
+# Otherwise just enter sh or zsh.
+else
+    if [ -f "/bin/zsh" ]; then
+        exec zsh
+    else
+        exec sh
+    fi
+fi
