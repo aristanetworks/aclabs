7c37947245fa22eebeff6ae9690665302ff6f6fc
Mitch Vaughan <mitch.vaughan@gmail.com>
2025-09-29 20:33:06 +0000

    add vxlan domain b alias.sh

---
+++ labs/techlib-vxlan-domain-b/addAliases.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set +e
+echo "alias B-SPINE1='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.101'" >> ~/.zshrc
+echo "alias B-SPINE2='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.102'" >> ~/.zshrc
+echo "alias B-SPINE3='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.103'" >> ~/.zshrc
+echo "alias B-SPINE4='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.104'" >> ~/.zshrc
+echo "alias B-LEAF1='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.105'" >> ~/.zshrc
+echo "alias B-LEAF2='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.106'" >> ~/.zshrc
+echo "alias B-LEAF3='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.107'" >> ~/.zshrc
+echo "alias B-LEAF4='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.108'" >> ~/.zshrc
+echo "alias B-LEAF5='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.109'" >> ~/.zshrc
+echo "alias B-LEAF6='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.110'" >> ~/.zshrc
+echo "alias B-LEAF7='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.111'" >> ~/.zshrc
+echo "alias B-LEAF8='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.112'" >> ~/.zshrc
+echo "alias HostB1='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.201'" >> ~/.zshrc
+echo "alias HostB2='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.202'" >> ~/.zshrc
+echo "alias HostB3='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.203'" >> ~/.zshrc
+echo "alias HostB4='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.204'" >> ~/.zshrc
+echo "alias HostB5='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.205'" >> ~/.zshrc
+echo "alias HostB6='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.206'" >> ~/.zshrc
+echo "alias HostB7='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.207'" >> ~/.zshrc
