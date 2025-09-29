d29e264199f7f368ce6138cca472b988fd478201
Mitch Vaughan <mitch.vaughan@gmail.com>
2025-09-29 19:17:53 +0000

    vxlan domain a alias

---
+++ labs/techlib-vxlan-domain-a/addAliases.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set +e
+echo "alias A-SPINE1='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.101'" >> ~/.zshrc
+echo "alias A-SPINE2='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.102'" >> ~/.zshrc
+echo "alias A-SPINE3='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.103'" >> ~/.zshrc
+echo "alias A-SPINE4='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.104'" >> ~/.zshrc
+echo "alias A-LEAF1='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.105'" >> ~/.zshrc
+echo "alias A-LEAF2='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.106'" >> ~/.zshrc
+echo "alias A-LEAF3='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.107'" >> ~/.zshrc
+echo "alias A-LEAF4='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.108'" >> ~/.zshrc
+echo "alias A-LEAF5='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.109'" >> ~/.zshrc
+echo "alias A-LEAF6='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.110'" >> ~/.zshrc
+echo "alias A-LEAF7='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.111'" >> ~/.zshrc
+echo "alias A-LEAF8='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.112'" >> ~/.zshrc
+echo "alias HostA1='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.201'" >> ~/.zshrc
+echo "alias HostA2='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.202'" >> ~/.zshrc
+echo "alias HostA3='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.203'" >> ~/.zshrc
+echo "alias HostA4='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.204'" >> ~/.zshrc
+echo "alias HostA5='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.205'" >> ~/.zshrc
+echo "alias HostA6='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.206'" >> ~/.zshrc
