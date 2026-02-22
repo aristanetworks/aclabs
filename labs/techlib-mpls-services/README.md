0425209c3962b4fb5130fb640c29c6e8bfec8d77
Mitch Vaughan <mitch@arista.com>
2026-02-22 14:19:30 -0500

    techlib mpls services topo lab (#132)
    * add mpls services topo
    
    * update lab_vars for mpls-services lab

---
+++ labs/techlib-mpls-services/README.md
@@ -0,0 +1,1 @@
+# MPLS Services Lab
---
+++ labs/techlib-mpls-services/addAliases.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set +e
+echo "alias EOS1='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.101'" >> ~/.zshrc
+echo "alias EOS2='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.102'" >> ~/.zshrc
+echo "alias EOS3='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.103'" >> ~/.zshrc
+echo "alias EOS4='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.104'" >> ~/.zshrc
+echo "alias EOS5='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.104'" >> ~/.zshrc
+echo "alias EOS6='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.104'" >> ~/.zshrc
+echo "alias EOS7='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.104'" >> ~/.zshrc
+echo "alias EOS8='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.104'" >> ~/.zshrc
+echo "alias Host1='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.201'" >> ~/.zshrc
+echo "alias Host2='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.202'" >> ~/.zshrc
+echo "alias Host3='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.203'" >> ~/.zshrc
+echo "alias Host4='sshpass -p admin ssh -o \"StrictHostKeyChecking no\" admin@172.100.100.204'" >> ~/.zshrc
