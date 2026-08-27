f9f908a96d802dab948c12150fcd3468810abb00
pa <pa@arista.com>
2026-08-27 15:55:55 +0000

    fixes

--- labs/anta-psirt/addAliases.sh
+++ labs/anta-psirt/addAliases.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 set +e
-
-echo "alias eos1='sshpass -p ${LABPASSPHRASE} ssh -o \"StrictHostKeyChecking no\" ${LABUSERNAME}@172.100.100.11'" >> ~/.zshrc
-echo "alias eos2='sshpass -p ${LABPASSPHRASE} ssh -o \"StrictHostKeyChecking no\" ${LABUSERNAME}@172.100.100.12'" >> ~/.zshrc
-echo "alias eos3='sshpass -p ${LABPASSPHRASE} ssh -o \"StrictHostKeyChecking no\" ${LABUSERNAME}@172.100.100.13'" >> ~/.zshrc
-echo "alias eos4='sshpass -p ${LABPASSPHRASE} ssh -o \"StrictHostKeyChecking no\" ${LABUSERNAME}@172.100.100.14'" >> ~/.zshrc
+echo "alias eos1='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.100.100.11'" >> ~/.zshrc
+echo "alias eos2='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.100.100.12'" >> ~/.zshrc
+echo "alias eos3='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.100.100.13'" >> ~/.zshrc
+echo "alias eos4='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.100.100.14'" >> ~/.zshrc
